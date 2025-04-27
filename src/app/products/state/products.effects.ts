6c3a6525d1d3d8fdbaf5ce5c4efd39e3b8690bdf
victor <gfeng005@uottawa.ca>
2025-04-27 14:17:01 -0400

    added error handling for the load product effect, and added effect for add product, update product, and delete product.

--- src/app/products/state/products.effects.ts
+++ src/app/products/state/products.effects.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { ProductsService } from '../products.service';
 import { ProductsAPIActions, ProductsPageActions } from './products.actions';
-import { concatMap, map } from 'rxjs';
+import { catchError, concatMap, exhaustMap, map, mergeMap, of } from 'rxjs';
+import { Product } from '../product.model';
 
 @Injectable()
 export class ProductEffects {
@@ -14,14 +15,59 @@ export class ProductEffects {
   loadProducts$ = createEffect(() =>
     this.actions$.pipe(
       ofType(ProductsPageActions.loadProducts),
-      concatMap(() =>
-        this.productsService
-          .getAll()
-          .pipe(
-            map((products) =>
-              ProductsAPIActions.productsLoadedSuccess({ products })
-            )
+      exhaustMap(() =>
+        this.productsService.getAll().pipe(
+          map((products) =>
+            ProductsAPIActions.productsLoadedSuccess({ products })
+          ),
+          catchError((error) =>
+            of(ProductsAPIActions.productsLoadedFail({ message: error }))
           )
+        )
+      )
+    )
+  );
+
+  addProduct$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(ProductsPageActions.addProduct),
+      mergeMap(({ product }) =>
+        this.productsService.add(product).pipe(
+          map((newProduct) =>
+            ProductsAPIActions.productAddedSuccess({ product: newProduct })
+          ),
+          catchError((error) =>
+            of(ProductsAPIActions.productAddedFail({ message: error }))
+          )
+        )
+      )
+    )
+  );
+
+  updateProduct$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(ProductsPageActions.updateProduct),
+      concatMap(({ product }) =>
+        this.productsService.update(product).pipe(
+          map(() => ProductsAPIActions.productUpdatedSuccess({ product })),
+          catchError((error) =>
+            of(ProductsAPIActions.productUpdatedFail({ message: error }))
+          )
+        )
+      )
+    )
+  );
+
+  deleteProduct$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(ProductsPageActions.deleteProduct),
+      mergeMap(({ id }) =>
+        this.productsService.delete(id).pipe(
+          map(() => ProductsAPIActions.productDeletedSuccess({ id })),
+          catchError((error) =>
+            of(ProductsAPIActions.productDeletedFail({ message: error }))
+          )
+        )
       )
     )
   );
